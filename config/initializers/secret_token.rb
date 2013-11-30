18dd5f4d8f3a7a5ba845ab54c8e991661b0ecf30
grant <grant@gitcafe.com>
2013-11-30 21:11:17 +0800

    init commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+GrantBlog::Application.config.secret_key_base = 'e53ce5dc1f2838e044c0065625a5fa1e2072a3c043a171f71b5234099b3c356b420ec04957460c0d0f4ea253b7dbee264faf04bef33c43923bc2cc553acf7a2d'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+GrantBlog::Application.config.session_store :cookie_store, key: '_grant_blog_session'
