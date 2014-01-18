b5230eaa527d8d7d67bfb1f17bad0fcb6607eaf7
grant <grant@gitcafe.com>
2014-01-18 23:25:10 +0800

    can edit blog template

--- app/controllers/admin/blog_templates_controller.rb
+++ app/controllers/admin/blog_templates_controller.rb
@@ -1,2 +1,9 @@
 class Admin::BlogTemplatesController < Admin::BaseController
+  def permitted_params
+    params.permit(:blog_template => [:title, :content])
+  end
+
+  def show
+    redirect_to admin_blog_templates_path
+  end
 end
--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,6 @@
 module ApplicationHelper
+
+  def admin_tabs_class(controller)
+    params[:controller] == controller ? "active" : ""
+  end
 end
