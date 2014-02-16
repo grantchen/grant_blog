a8c10a786f085c7b22584d050bc1fecb47b1f857
grant <grant@gitcafe.com>
2014-02-16 22:36:38 +0800

    reset foundation icon

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -60,6 +60,7 @@ GrantBlog::Application.configure do
   # Precompile additional assets.
   # application.js, application.css, and all non-JS/CSS in app/assets folder are already added.
   # config.assets.precompile += %w( search.js )
+  config.assets.precompile += %w(vendor/assets/stylesheets/foundation-icons/*.woff)
 
   # Ignore bad email addresses and do not raise email delivery errors.
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.
